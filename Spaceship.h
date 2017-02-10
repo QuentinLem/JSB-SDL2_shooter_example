51c1b7f6e79afddf96fbeaeedc7c19ebe24331c0
Morpheus <quentin.lemonnier.info@gmail.com>
2017-02-10 21:34:27 +0100

    First blank files

---
+++ Spaceship.h
@@ -0,0 +1,22 @@
+#ifndef _SPACESHIP_
+#define _SPACESHIP_
+
+#include <string>
+
+class Spaceship {
+private:
+  int aLife;
+  float aPositionX;
+  float aPositionY;
+  float aAccelaration;
+
+  public:
+    void Shoot();
+    bool IsAlive();
+    
+    int GetLife();
+    void SetLife(int lifeValue);
+};
+
+
+#endif
